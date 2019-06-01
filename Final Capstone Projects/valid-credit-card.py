e5775ec70e276c7a61b1a46484c64f40eab2d269
mikehowell <_mike@outlook.co.nz>
2019-06-02 10:07:33 +1200

    simplyfied the credit card validation script

--- Final Capstone Projects/valid-credit-card.py
+++ Final Capstone Projects/valid-credit-card.py
@@ -1,23 +1,32 @@
-credit_card = int(input("Please enter a credit card number to check: "))
-credit_card_length = len(str(credit_card))
+"""
+How to check if a credit card is valid:
+http://www.tech-faq.com/how-to-determine-if-i-have-a-valid-credit-card-number.html
+"""
+
+credit_card = input("Please enter a credit card number to check: ")
+credit_card_length = len(credit_card)
+even_digits = credit_card[1::2]
+odd_digits = credit_card[::2]
 
 credit_card_checksum = 0
 
 if credit_card_length % 2 == 0:
-    for i in range(1, credit_card_length + 1):
-        if i % 2 != 0:
-            credit_card_checksum += 2 * int(str(credit_card)[i - 1])
-        else:
-            credit_card_checksum += int(str(credit_card)[i - 1])
+    for num in even_digits:
+        credit_card_checksum += int(num) * 2
+
+    for num in odd_digits:
+        credit_card_checksum += int(num)
 else:
-    for i in range(1, credit_card_length + 1):
-        if i % 2 == 0:
-            credit_card_checksum += 2 * int(str(credit_card)[i - 1])
-        else:
-            credit_card_checksum += int(str(credit_card)[i - 1])
+    for num in even_digits:
+        credit_card_checksum += int(num)
+
+    for num in odd_digits:
+        credit_card_checksum += int(num) * 2
 
 if credit_card_checksum > 9:
     credit_card_checksum -= 9
+
+print("check sum = : {}".format(credit_card_checksum))
 
 if credit_card_checksum % 10 == 0:
     print("Valid Credit Card Number")
